82d950192a1cedd500ecd482272022025afddf17
Rayson Zhu <vfreex@gmail.com>
2015-09-22 16:01:34 +0800

    update TPS template

--- src/chartgeneration-perftest/java/chartgeneration/configtemplate/JmeterTPSChartTemplate.java
+++ src/chartgeneration-perftest/java/chartgeneration/configtemplate/JmeterTPSChartTemplate.java
@@ -26,7 +26,7 @@ public class JmeterTPSChartTemplate extends Chart2DTemplateWithIntervalBase {
 		FieldSelector xField = new AddTransformSelector(timestampField, rtField);
 		rules = new ArrayList<Chart2DSeriesConfigRule>();
 		rules.add(new Chart2DSeriesConfigRule("^TX-(.+)-S$", "$1-Success", "", getLabelField(),
-				xField, null, new CountCalculation(interval, 1000.0 / interval, true), true, false, false));
+				xField, null, new CountCalculation(interval, 1000.0 / interval, false), true, false, false));
 		rules.add(new Chart2DSeriesConfigRule("^TX-(.+)-F$", "$1-Failure", "", getLabelField(),
 				xField, null, new CountCalculation(interval, 1000.0 / interval, true), true, false, false));
 		Chart2DConfig cfg =  createConfig("TPS over Time", "Time", "TPS", rules, AxisMode.TIME);
